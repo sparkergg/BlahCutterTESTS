53b57859ec7f35fd7aaaeb5d149b3cd7714f4d34
Gonzo Vera <gonza.fma@gmail.com>
2024-11-07 14:12:12 -0300

    Update app_cloud.py

--- app_cloud.py
+++ app_cloud.py
@@ -147,191 +147,201 @@ def video_downloader(url):
 
 # Interfaz de Streamlit
 import streamlit as st
-st.title("BlahCutter app")
+username=st.secrets["login"]["user"]
+password=st.secrets["login"]["pass"]
+st.title("BlahCutter")
 st.write("Transcriptor de audio y video")
-video_url = st.text_input("Ingresa la URL de YouTube para descargar el video")
-uploaded_file = st.file_uploader("Carga el archivo multimedia", type=["mp3", "mp4", "wav", "m4a", "ogg", "avi", "mov"])
-if video_url:
-    temp_file_path = os.path.join(temp_folder, "archivo_trabajado.mp4")
-    if "temp_file_path" not in st.session_state:
-        video_downloader(video_url)
-        st.session_state['temp_file_path']=temp_file_path
-        st.session_state['file_name']="archivo_trabajado.mp4"
-        st.session_state['is_video'] = temp_file_path
-        if "transcription" not in st.session_state:
-            st.write("Procesando transcripción...")
-            transcription, transcription_data = transcribe_and_format(temp_file_path)
-            st.session_state['transcription'] = transcription
-            st.session_state['transcription_data'] = transcription_data
-        else:
-            st.write("Transcripción cargada desde la sesión.")
-            transcription = st.session_state['transcription']
-            transcription_data = st.session_state['transcription_data']
-    st.video(temp_file_path)
-    st.session_state['reproductor'] = "reproductor"
 
-elif uploaded_file:
-    temp_file_path = os.path.join("temp_dir", uploaded_file.name)
-    st.session_state['temp_file_path'] = temp_file_path
-    st.session_state['file_name']=uploaded_file.name
-    if not os.path.exists("temp_dir"):
-        os.makedirs("temp_dir")
-    with open(temp_file_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    is_video = uploaded_file.name.endswith(('mp4', '.mov', '.avi'))
-    if is_video:
-        st.video(temp_file_path)
-        st.session_state['is_video'] = temp_file_path
-    else:
-        st.audio(temp_file_path)
-        st.session_state['reproductor'] = "reproductor"
-    if "transcription" not in st.session_state:
-        st.write("Procesando transcripción...")
-        transcription, transcription_data = transcribe_and_format(temp_file_path)
-        st.session_state['transcription'] = transcription
-        st.session_state['transcription_data'] = transcription_data
-    else:
-        st.write("Transcripción cargada desde la sesión.")
-        transcription = st.session_state['transcription']
-        transcription_data = st.session_state['transcription_data']
+input_user = st.text_input("Usuario")
+input_password = st.text_input("Contraseña", type="password")
 
-    # Mostrar transcripción en la aplicación
-if 'transcription' in session_state:
-    st.text_area("Transcripción", st.session_state.transcription, height=300)
+if st.button("Iniciar sesión"):
+    if input_user == username and input_password == password:
+        video_url = st.text_input("Ingresa la URL de YouTube para descargar el video")
+        uploaded_file = st.file_uploader("Carga el archivo multimedia", type=["mp3", "mp4", "wav", "m4a", "ogg", "avi", "mov"])
+        if video_url:
+            temp_file_path = os.path.join(temp_folder, "archivo_trabajado.mp4")
+            if "temp_file_path" not in st.session_state:
+                video_downloader(video_url)
+                st.session_state['temp_file_path']=temp_file_path
+                st.session_state['file_name']="archivo_trabajado.mp4"
+                st.session_state['is_video'] = temp_file_path
+                if "transcription" not in st.session_state:
+                    st.write("Procesando transcripción...")
+                    transcription, transcription_data = transcribe_and_format(temp_file_path)
+                    st.session_state['transcription'] = transcription
+                    st.session_state['transcription_data'] = transcription_data
+                else:
+                    st.write("Transcripción cargada desde la sesión.")
+                    transcription = st.session_state['transcription']
+                    transcription_data = st.session_state['transcription_data']
+            st.video(temp_file_path)
+            st.session_state['reproductor'] = "reproductor"
 
-# Nombre base sin la extensión
-if 'file_name' in session_state:
-    base_name = os.path.splitext(st.session_state.file_name)[0]
+        elif uploaded_file:
+            temp_file_path = os.path.join("temp_dir", uploaded_file.name)
+            st.session_state['temp_file_path'] = temp_file_path
+            st.session_state['file_name']=uploaded_file.name
+            if not os.path.exists("temp_dir"):
+                os.makedirs("temp_dir")
+            with open(temp_file_path, "wb") as f:
+                f.write(uploaded_file.getbuffer())
+            is_video = uploaded_file.name.endswith(('mp4', '.mov', '.avi'))
+            if is_video:
+                st.video(temp_file_path)
+                st.session_state['is_video'] = temp_file_path
+            else:
+                st.audio(temp_file_path)
+                st.session_state['reproductor'] = "reproductor"
+            if "transcription" not in st.session_state:
+                st.write("Procesando transcripción...")
+                transcription, transcription_data = transcribe_and_format(temp_file_path)
+                st.session_state['transcription'] = transcription
+                st.session_state['transcription_data'] = transcription_data
+            else:
+                st.write("Transcripción cargada desde la sesión.")
+                transcription = st.session_state['transcription']
+                transcription_data = st.session_state['transcription_data']
 
-    # Descargar transcripción como .txt y .json
-    text_col1, text_col2 = st.columns(2)
-    with text_col1:
-            st.download_button(
-                label="Descargar transcripción como .txt",
-                data=st.session_state.transcription,
-                file_name=f"transcripcion {base_name}.txt",
-                mime="text/plain"
-            )
-    with text_col2:
-        formatted_transcription = format_transcription(st.session_state.transcription_data)
-        formatted_json = json.dumps(formatted_transcription, indent=4, ensure_ascii=False)
-        st.download_button(
-            label="Descargar transcripción como .json",
-            data=formatted_json,
-            file_name=f"transcripcion {base_name}.json",
-            mime="application/json"
-        )
+            # Mostrar transcripción en la aplicación
+        if 'transcription' in session_state:
+            st.text_area("Transcripción", st.session_state.transcription, height=300)
 
-    # Barra de busqueda de texto
-    search_text = st.text_input("Buscar en la transcripción")
-    if st.button("Buscar") or search_text:
-        search_results = search_in_transcription(search_text, st.session_state.transcription_data)
+        # Nombre base sin la extensión
+        if 'file_name' in session_state:
+            base_name = os.path.splitext(st.session_state.file_name)[0]
 
-        if search_results:
-            st.write(f"Resultados para '{search_text}':")
-            for result in search_results:
-                st.write(
-                    f"[{result['start']} - {result['end']}] Speaker {result['speaker']}: {result['text']}")
-
-            selected_result = search_results[0]
-
-            cut_audio_segment = cut_audio(st.session_state.temp_file_path, selected_result['start'], selected_result['end'])
-            cut_audio_path = os.path.join("temp_dir", f"cut_{st.session_state.file_name}")
-            cut_audio_segment.export(cut_audio_path, format="mp3")
-
-            audio_col1, audio_col2 = st.columns(2)
-
-            # Descargar fragmento de audio
-            audio_col1.download_button(
-            label="Descargar fragmento de audio",
-            data=open(cut_audio_path, "rb").read(),
-            file_name=f"fragmento {base_name}.mp3",
-            mime="audio/mp3"
-            )
-
-            if st.session_state.is_video:
-                cut_video_segment = cut_video(st.session_state.temp_file_path, selected_result['start'], selected_result['end'])
-                cut_video_path = os.path.join("temp_dir", f"fragmento_{base_name}.mp4")
-                cut_video_segment.write_videofile(cut_video_path, codec="libx264", audio_codec="aac")
-
-                # Descargar fragmento de video
-                audio_col2.download_button(
-                label="Descargar fragmento de video",
-                data=open(cut_video_path, "rb").read(),
-                file_name=f"fragmento {base_name}.mp4",
-                mime="video/mp4"
+            # Descargar transcripción como .txt y .json
+            text_col1, text_col2 = st.columns(2)
+            with text_col1:
+                    st.download_button(
+                        label="Descargar transcripción como .txt",
+                        data=st.session_state.transcription,
+                        file_name=f"transcripcion {base_name}.txt",
+                        mime="text/plain"
+                    )
+            with text_col2:
+                formatted_transcription = format_transcription(st.session_state.transcription_data)
+                formatted_json = json.dumps(formatted_transcription, indent=4, ensure_ascii=False)
+                st.download_button(
+                    label="Descargar transcripción como .json",
+                    data=formatted_json,
+                    file_name=f"transcripcion {base_name}.json",
+                    mime="application/json"
                 )
 
-    # Interfaz para seleccionar interlocutor
-    speaker_ids = set(entry['speaker'] for entry in st.session_state.transcription_data)
-    selected_speaker = st.selectbox("Selecciona un interlocutor", options=["Seleccionar"] + list(speaker_ids))
+            # Barra de busqueda de texto
+            search_text = st.text_input("Buscar en la transcripción")
+            if st.button("Buscar") or search_text:
+                search_results = search_in_transcription(search_text, st.session_state.transcription_data)
 
-    if selected_speaker and selected_speaker != "Seleccionar":
-        speaker_statements = [entry['text'] for entry in st.session_state.transcription_data if entry['speaker'] == selected_speaker]
-        concatenated_text = " ".join(speaker_statements)
-        st.text_area(f"Declaraciones concatenadas del interlocutor {selected_speaker}", concatenated_text,
-                height=150)
+                if search_results:
+                    st.write(f"Resultados para '{search_text}':")
+                    for result in search_results:
+                        st.write(
+                            f"[{result['start']} - {result['end']}] Speaker {result['speaker']}: {result['text']}")
 
-        # Descargar declaraciones del speaker como .txt y .json
-        text_col3, text_col4 = st.columns(2)
-        with text_col3:
-            st.download_button(
-            label="Descargar declaraciones como .txt",
-            data=concatenated_text,
-            file_name=f"declaraciones {selected_speaker}.txt",
-            mime="text/plain"
-            )
-        with text_col4:
-            filtered_transcription = [
-            {
-            "speaker": entry['speaker'],
-            "start": entry['start'],
-            "end": entry['end'],
-            "text": entry['text']
-            }
-            for entry in st.session_state.transcription_data if entry['speaker'] == selected_speaker
-            ]
-            filtered_json = json.dumps(filtered_transcription, indent=4, ensure_ascii=False)
-            st.download_button(
-            label="Descargar declaraciones como .json",
-            data=filtered_json,
-            file_name=f"declaraciones {selected_speaker}.json",
-            mime="application/json"
-            )
+                    selected_result = search_results[0]
 
-        # Generar fragmentos de audio y video para el interlocutor seleccionado
-        speaker_audio = cut_audio(st.session_state.temp_file_path, 0, 0)
-        speaker_video = None
-        for entry in st.session_state.transcription_data:
-            if entry['speaker'] == selected_speaker:
-                fragment = cut_audio(st.session_state.temp_file_path, entry['start'], entry['end'])
-                speaker_audio += fragment
-                if st.session_state.is_video:
-                    video_fragment = cut_video(st.session_state.temp_file_path, entry['start'], entry['end'])
-                    if speaker_video is None:
-                        speaker_video = video_fragment
-                    else:
-                        from moviepy.editor import concatenate_videoclips
-                        speaker_video = concatenate_videoclips([speaker_video, video_fragment])
+                    cut_audio_segment = cut_audio(st.session_state.temp_file_path, selected_result['start'], selected_result['end'])
+                    cut_audio_path = os.path.join("temp_dir", f"cut_{st.session_state.file_name}")
+                    cut_audio_segment.export(cut_audio_path, format="mp3")
 
-        # Descargar declaraciones como audio
-        speaker_audio_path = os.path.join("temp_dir", f"declaraciones_{selected_speaker}.mp3")
-        speaker_audio.export(speaker_audio_path, format="mp3")
-        media_col1, media_col2 = st.columns(2)
-        media_col1.download_button(
-        label=f"Descargar declaraciones como audio",
-        data=open(speaker_audio_path, "rb").read(),
-        file_name=f"declaraciones {selected_speaker}.mp3",
-        mime="audio/mp3"
-        )
+                    audio_col1, audio_col2 = st.columns(2)
 
-        # Descargar declaraciones como video (si es un video)
-        if st.session_state.is_video and speaker_video:
-            speaker_video_path = os.path.join("temp_dir", f"declaraciones_{selected_speaker}.mp4")
-            speaker_video.write_videofile(speaker_video_path, codec="libx264", audio_codec="aac")
-            media_col2.download_button(
-            label="Descargar declaraciones como video",
-            data=open(speaker_video_path, "rb").read(),
-            file_name=f"declaraciones {selected_speaker}.mp4",
-            mime="video/mp4"
-            )
+                    # Descargar fragmento de audio
+                    audio_col1.download_button(
+                    label="Descargar fragmento de audio",
+                    data=open(cut_audio_path, "rb").read(),
+                    file_name=f"fragmento {base_name}.mp3",
+                    mime="audio/mp3"
+                    )
+
+                    if st.session_state.is_video:
+                        cut_video_segment = cut_video(st.session_state.temp_file_path, selected_result['start'], selected_result['end'])
+                        cut_video_path = os.path.join("temp_dir", f"fragmento_{base_name}.mp4")
+                        cut_video_segment.write_videofile(cut_video_path, codec="libx264", audio_codec="aac")
+
+                        # Descargar fragmento de video
+                        audio_col2.download_button(
+                        label="Descargar fragmento de video",
+                        data=open(cut_video_path, "rb").read(),
+                        file_name=f"fragmento {base_name}.mp4",
+                        mime="video/mp4"
+                        )
+
+            # Interfaz para seleccionar interlocutor
+            speaker_ids = set(entry['speaker'] for entry in st.session_state.transcription_data)
+            selected_speaker = st.selectbox("Selecciona un interlocutor", options=["Seleccionar"] + list(speaker_ids))
+
+            if selected_speaker and selected_speaker != "Seleccionar":
+                speaker_statements = [entry['text'] for entry in st.session_state.transcription_data if entry['speaker'] == selected_speaker]
+                concatenated_text = " ".join(speaker_statements)
+                st.text_area(f"Declaraciones concatenadas del interlocutor {selected_speaker}", concatenated_text,
+                        height=150)
+
+                # Descargar declaraciones del speaker como .txt y .json
+                text_col3, text_col4 = st.columns(2)
+                with text_col3:
+                    st.download_button(
+                    label="Descargar declaraciones como .txt",
+                    data=concatenated_text,
+                    file_name=f"declaraciones {selected_speaker}.txt",
+                    mime="text/plain"
+                    )
+                with text_col4:
+                    filtered_transcription = [
+                    {
+                    "speaker": entry['speaker'],
+                    "start": entry['start'],
+                    "end": entry['end'],
+                    "text": entry['text']
+                    }
+                    for entry in st.session_state.transcription_data if entry['speaker'] == selected_speaker
+                    ]
+                    filtered_json = json.dumps(filtered_transcription, indent=4, ensure_ascii=False)
+                    st.download_button(
+                    label="Descargar declaraciones como .json",
+                    data=filtered_json,
+                    file_name=f"declaraciones {selected_speaker}.json",
+                    mime="application/json"
+                    )
+
+                # Generar fragmentos de audio y video para el interlocutor seleccionado
+                speaker_audio = cut_audio(st.session_state.temp_file_path, 0, 0)
+                speaker_video = None
+                for entry in st.session_state.transcription_data:
+                    if entry['speaker'] == selected_speaker:
+                        fragment = cut_audio(st.session_state.temp_file_path, entry['start'], entry['end'])
+                        speaker_audio += fragment
+                        if st.session_state.is_video:
+                            video_fragment = cut_video(st.session_state.temp_file_path, entry['start'], entry['end'])
+                            if speaker_video is None:
+                                speaker_video = video_fragment
+                            else:
+                                from moviepy.editor import concatenate_videoclips
+                                speaker_video = concatenate_videoclips([speaker_video, video_fragment])
+
+                # Descargar declaraciones como audio
+                speaker_audio_path = os.path.join("temp_dir", f"declaraciones_{selected_speaker}.mp3")
+                speaker_audio.export(speaker_audio_path, format="mp3")
+                media_col1, media_col2 = st.columns(2)
+                media_col1.download_button(
+                label=f"Descargar declaraciones como audio",
+                data=open(speaker_audio_path, "rb").read(),
+                file_name=f"declaraciones {selected_speaker}.mp3",
+                mime="audio/mp3"
+                )
+
+                # Descargar declaraciones como video (si es un video)
+                if st.session_state.is_video and speaker_video:
+                    speaker_video_path = os.path.join("temp_dir", f"declaraciones_{selected_speaker}.mp4")
+                    speaker_video.write_videofile(speaker_video_path, codec="libx264", audio_codec="aac")
+                    media_col2.download_button(
+                    label="Descargar declaraciones como video",
+                    data=open(speaker_video_path, "rb").read(),
+                    file_name=f"declaraciones {selected_speaker}.mp4",
+                    mime="video/mp4"
+                    )
+    else:
+        st.error("Usuario o contraseña incorrectos")
